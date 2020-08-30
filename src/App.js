bd7151aacffec9edecedfbf38b02a99558e32e86
Umer Khatab <abbasiumerkhatab@gmail.com>
2020-08-30 17:05:03 +0500

    added countries statistics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NavBar from './components/NavBar.js'
+import FootNav from './components/FootNav'
 import InfoPanel from './components/InfoPanel.js'
 import './App.css';
 
 function App() {
+  const screenConfig = useState(0);
   return (
     <div>
+
       <NavBar />
       <InfoPanel />
+      <FootNav screenConfig={screenConfig} />
     </div>
   );
 }
